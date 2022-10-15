7f665ee47f47bbb74b007511bc1dd4c2bceef8eb
Murran <claytoo@sfcerenity.org>
2022-10-15 12:54:37 +0700

    add: implementing multiplayer to game

--- Assets/Editor/csFolderStructure.cs
+++ Assets/Editor/csFolderStructure.cs
@@ -1,76 +1,79 @@
 /* 
     Cerenity Studio Folder Structure C# script for Unity Projects 
-    Copyrighted ©2020 - 2022
+    Copyright ©2018 - 2022
 */
 using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
 using System.IO;
 
-public class csFolderStructure : EditorWindow
+namespace CerenityStudio
 {
-    private static string projectName = "_MAIN / _DEV ?";
-    [MenuItem("Asset/Create Default Folders")]
-    private static void SetUpFolders()
+    public class csFolderStructure : EditorWindow
     {
-        csFolderStructure window = ScriptableObject.CreateInstance<csFolderStructure>();
-        window.position = new Rect(Screen.width / 2, Screen.height / 2, 400, 150);
-        window.ShowPopup();
-    }
-
-    private static void CreateAllFolders()
-    {
-        List<string> folders = new List<string>
+        private static string projectName = "Enter a folder name";
+        [MenuItem("Cerenity Studio/Tools/Setup/Create Default Folders")]
+        private static void SetUpFolders()
         {
-            "_Animations",
-            "_Audio",
-            "_Editor",
-            "_Materials",
-            "_Meshes",
-            "_Prefabs",
-            "_Scripts",
-            "_Scenes",
-            "_Shaders",
-            "_Textures",
-            "_UI"
-        };
-
-        foreach (string folder in folders)
-        {
-            if (!Directory.Exists("Assets/" + folder))
-            {
-                Directory.CreateDirectory("Assets/" + projectName + "/" + folder);
-            }
+            csFolderStructure window = ScriptableObject.CreateInstance<csFolderStructure>();
+            window.position = new Rect(Screen.width / 2, Screen.height / 2, 400, 150);
+            window.ShowPopup();
         }
 
-        List<string> uiFolders = new List<string>
+        private static void CreateAllFolders()
         {
-            "_Assets",
-            "_Fonts",
-            "_Icon"
-        };
-
-        foreach (string subfolder in uiFolders)
-        {
-            if(!Directory.Exists("Assets/" + projectName + "/_UI/" + subfolder))
+            List<string> folders = new List<string>
             {
-                Directory.CreateDirectory("Assets/" + projectName + "/_UI/" + subfolder);
+                "Animations",
+                "Audio",
+                "Editor",
+                "Materials",
+                "Meshes",
+                "Prefabs",
+                "Scripts",
+                "Scenes",
+                "Shaders",
+                "Textures",
+                "UI"
+            };
+
+            foreach (string folder in folders)
+            {
+                if (!Directory.Exists("Assets/" + folder))
+                {
+                    Directory.CreateDirectory("Assets/" + projectName + "/" + folder);
+                }
             }
+
+            List<string> uiFolders = new List<string>
+            {
+                "Assets",
+                "Fonts",
+                "Icon"
+            };
+
+            foreach (string subfolder in uiFolders)
+            {
+                if(!Directory.Exists("Assets/" + projectName + "/UI/" + subfolder))
+                {
+                    Directory.CreateDirectory("Assets/" + projectName + "/UI/" + subfolder);
+                }
+            }
+
+            AssetDatabase.Refresh();
         }
 
-        AssetDatabase.Refresh();
-    }
-
-    private void OnGUI()
-    {
-        EditorGUILayout.LabelField("Insert the Project name used as the root folder");
-        projectName = EditorGUILayout.TextField("Project Name: ", projectName);
-        this.Repaint();
-        GUILayout.Space(70);
-        if (GUILayout.Button("Generate!"))
+        private void OnGUI()
         {
-            CreateAllFolders();
-            this.Close();
+            EditorGUILayout.LabelField("Insert the Project name used as the root folder");
+            projectName = EditorGUILayout.TextField("Project Name: ", projectName);
+            this.Repaint();
+            GUILayout.Space(70);
+            if (GUILayout.Button("Generate!"))
+            {
+                CreateAllFolders();
+                this.Close();
+            }
         }
     }
 }
---
+++ Assets/_MAIN/Scripts/GameManager.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    [Header("UI")]
+    public GameObject uI_InformPanelGameobject;
+    public TextMeshProUGUI uI_InformText;
+    public GameObject searchForGamesButtonGameobject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uI_InformPanelGameobject.SetActive(true);
+        uI_InformText.text = "Search For Games to BATTLE!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region UI Callback Methods
+    public void JoinRandomRoom()
+    {
+        uI_InformText.text = "Searching for available rooms...";
+
+        PhotonNetwork.JoinRandomRoom();
+
+        searchForGamesButtonGameobject.SetActive(false);
+    }
+
+    #endregion
+
+    #region PHOTON Callback Methods
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        uI_InformText.text = message;
+        CreateAndJoinRoom();
+
+#if UNITY_EDITOR
+        Debug.Log(message);
+#endif
+    }
+
+    public override void OnJoinedRoom()
+    {
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        {
+            uI_InformText.text = "Joined to " + PhotonNetwork.CurrentRoom.Name + ". Waiting for other players...";
+        }
+        else
+        {
+            uI_InformText.text = "Joined to " + PhotonNetwork.CurrentRoom.Name;
+            StartCoroutine(DeactivateAfterSeconds(uI_InformPanelGameobject, 2.0f));
+        }
+
+#if UNITY_EDITOR
+        Debug.Log(PhotonNetwork.NickName + " joined to " + PhotonNetwork.CurrentRoom.Name);
+#endif
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        uI_InformText.text = newPlayer.NickName + " joined to " + PhotonNetwork.CurrentRoom.Name + " Player count " + PhotonNetwork.CurrentRoom.PlayerCount;
+
+        StartCoroutine(DeactivateAfterSeconds(uI_InformPanelGameobject, 2.0f));
+
+#if UNITY_EDITOR
+        Debug.Log(newPlayer.NickName + " joined to " + PhotonNetwork.CurrentRoom.Name + " Player count " + PhotonNetwork.CurrentRoom.PlayerCount);
+#endif
+    }
+
+    #endregion
+
+    #region PRIVATE Methods
+    void CreateAndJoinRoom()
+    {
+        string randomRoomName = "Room " + Random.Range(0, 1000);
+
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
+
+        // Creating the room
+        PhotonNetwork.CreateRoom(randomRoomName, roomOptions);
+    }
+
+    IEnumerator DeactivateAfterSeconds(GameObject _gameObject, float _seconds)
+    {
+        yield return new WaitForSeconds(_seconds);
+        _gameObject.SetActive(false);
+    }
+    #endregion
+}
